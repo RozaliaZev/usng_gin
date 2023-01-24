4871674e800d85e0780c5a074063123718322857
Zevyakina Rozalia <rozalka.zevyakina@yandex.ru>
2023-01-25 00:13:00 +0300

    update

--- pkg/middlewares/middlewares.go
+++ pkg/middlewares/middlewares.go
@@ -1,9 +1,6 @@
 package middlewares
 
 import (
-	//"log"
-	//"net/http"
-	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -15,7 +12,6 @@ func SetHeader() gin.HandlerFunc {
 	start := time.Now()
 	header := c.Request.Header.Get("X-PING") 
 	if header == "ping" {
-		log.Println("бакаки")
 		c.Writer.Header().Set("X-PONG", "pong")
 	}
 	time.Since(start)
